667786556906f28ea000c905f6c933447de87566
harikrish112 <harikrishnanviswanathan406@gmail.com>
2023-09-29 20:00:32 +0530

    class added

---
+++ src/com/sample/Geetha.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+public class Geetha {
+    public static void main(String[] args) {
+		
+    	//SET THE PATH TO THE CHROMEDRIVER 
+    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hari\\Downloads\\chromedriver_win32");
+    	
+    	//create a new instance of chromedriver
+    	
+	}
+	
+}
